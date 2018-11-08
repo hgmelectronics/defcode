4db81c91b578ee7a94369375ddbfc871619a23f7
Doug Brunner <doug@epicycloidengr.com>
2018-11-08 11:49:32 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Introduction
+
+A loader for JS code generators that passes them the contents of another file (the definition file). Code generators must be modules that export a single function, as in:
+```
+module.exports = (def) => {
+    // do stuff and return a string
+}
+```
+It rewrites output files only when they change, to minimize the work done by build systems like `make` that use file modification time to determine the need for rebuilding. Generators should output text using newlines; `defcode` will convert them to the local system's EOL.
+
+# Usage
+
+`defcode [options] <generator source file> ...`
+
+Multiple generator sources can be passed on one command line. Note that this does not make sense with the `-o`,`--output` option, so all generator sources need to be named `<output file name>.js` to pass multiple sources on one command line.
+
+## Options
+
+### `-d`, `--def`
+Specifies the definition file to load. This option is required.
+| File extension | Parsing | Parameter type passed to generator |
+| --- | --- | --- |
+| `.json` | JSON | Object |
+| `.yaml` | YAML | Object |
+| `.yml` | YAML | Object |
+| Other/none | None | String |
+
+### `-o`, `--output`
+Specifies the output file to be written. The default is the name of the generator source file with the `.js` stripped from the end.
+
+## Examples
+
+* `defcode -d def.yaml MySource.cpp.js`: reads `def.yaml`, parses it as YAML, loads `MySource.cpp.js` and executes its exported function with the parsed YAML as an object, and writes the returned string to `MySource.cpp`.
---
+++ index.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const os = require('os');
+
+if (require.main === module) {
+    commander
+        .usage('[options] <generator source file> ...')
+        .option('-d --def [def.yaml]')
+        .option('-o --output [outputfile]', 'Output file name; not valid with multiple generator sources')
+        .parse(process.argv);
+
+    if (commander.args.length < 1 || !commander.def || (commander.args.length > 1 && commander.output)) {
+        commander.outputHelp();
+        process.exitCode = 1;
+    }
+    else {
+        let def = fs.readFileSync(commander.def, { encoding: 'utf8' });
+        if (/\.json$/i.exec(commander.def)) {
+            def = JSON.parse(def);
+        }
+        else if (/\.ya?ml$/i.exec(commander.def)) {
+            def = yaml.safeLoad(def);
+        }
+
+        for (const src of commander.args) {
+            const generator = require(fs.realpathSync(src));
+            if (!generator) {
+                console.error('Could not require() ' + src);
+                process.exitCode = 1;
+                break;
+            }
+
+            let content;
+            try {
+                content = generator(def);
+            }
+            catch (err) {
+                console.error(err);
+                process.exitCode = 1;
+                break;
+            }
+
+            const target = commander.output || src.replace(/\.[^.]*$/, ''); // strip final extension
+
+            content = content.replace(/\n/g, os.EOL);
+
+            let existing;
+            try {
+                existing = fs.readFileSync(target, { encoding: 'utf8' });
+            }
+            catch (err) {
+                if (err.code !== 'ENOENT') {
+                    console.warn(err);
+                }
+            }
+            if (existing === content) {
+                console.log(`${target} is up to date`);
+            }
+            else {
+                if (existing) {
+                    console.log(`${target} is out of date, updating`);
+                }
+                else {
+                    console.log(`creating ${target}`);
+                }
+                try {
+                    fs.writeFileSync(target, content, { encoding: 'utf8' });
+                }
+                catch (err) {
+                    console.error(err);
+                    process.exitCode = 1;
+                    break;
+                }
+            }
+        }
+    }
+}
